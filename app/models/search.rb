a9dce1125f86b28f593fa1b494aca5c36cce3510
chaudard <donnetonfalzard+github@gmail.com>
2018-06-05 14:56:01 +0200

    associations, validations et controllers

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,6 @@
 class Search < ApplicationRecord
+  has_many :articles
+  validates :keywords, presence: true
+  validates :input_address, presence: true
+  validates :distance, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'searches/new'
+  get 'searches/create'
+  get 'articles/index'
+  get 'articles/show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
