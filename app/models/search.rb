48198bfc6fa1c3be662825f2bbd7e2649c435447
chaudard <donnetonfalzard+github@gmail.com>
2018-06-05 15:06:13 +0200

    routes et search distances

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,4 +3,8 @@ class Search < ApplicationRecord
   validates :keywords, presence: true
   validates :input_address, presence: true
   validates :distance, presence: true
+
+  def self.distances
+    [10, 25, 50, 75]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'searches/new'
-  get 'searches/create'
-  get 'articles/index'
-  get 'articles/show'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :searches, only: [:new, :create] do
+    resources :articles, only: [:index, :show]
+  end
+
 end
