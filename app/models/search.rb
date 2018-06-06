fd5893dde40549cce1bc2e76e749db2ba4261120
simon levy <simon94700@hotmail.fr>
2018-06-06 13:08:29 +0200

    autocomplete home page

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,8 @@ class Search < ApplicationRecord
   validates :keywords, presence: true
   validates :input_address, presence: true
   validates :distance, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def self.distances
     [10, 25, 50, 75]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_121620) do
+ActiveRecord::Schema.define(version: 2018_06_06_085936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2018_06_05_121620) do
     t.integer "distance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "stocks", force: :cascade do |t|
