a085953d26eba22984375b7745451e3bb35591e2
chaudard <donnetonfalzard+github@gmail.com>
2018-06-08 13:52:33 +0200

    Insertion de la recherche des stores dans la recherche

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,7 @@
 class Store < ApplicationRecord
   belongs_to :provider, optional: true
   has_many :schedules
+  validates :longitude, :uniqueness => {:scope => [:latitude, :name]}
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_092541) do
+ActiveRecord::Schema.define(version: 2018_06_08_100433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_06_06_092541) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "city"
   end
 
   create_table "stocks", force: :cascade do |t|
