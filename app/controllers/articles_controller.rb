26cfbfa6129a3623f10a31117fbdd2df1db0bcf7
chaudard <donnetonfalzard+github@gmail.com>
2018-06-09 22:01:46 +0200

    correction pour markers sur view + retrait des articles qui n'ont pas de store à proximité (impact index articles)
    gst

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :set_search_and_near_stores, only: [:index, :show]
+
   def index
-    search = Search.find(params[:search_id])
-    # prendre les articles qui ont du stock
-    @articles = search.articles.select { |a| a.stocks.map { |s| s.available}.include?(true) }
+    # listons les providers qui se trouvent dans le rayon de recherche
+    near_stores_providers = []
+    @near_stores.each do |store|
+      near_stores_providers << store.provider if !near_stores_providers.include?(store.provider)
+    end
+    # prendre les articles qui ont du stock et dont le fournisseur a un store à proximité
+    @articles = @search.articles.select { |a| a.stocks.map { |s| s.available}.include?(true) && near_stores_providers.include?(a.provider)}
     return false if @articles == nil
 
     stores = []
-    near_stores = []
-    stock_stores = []
 
     #find stores from enseigne with articles with stock
     @articles.each do |article|
@@ -15,30 +19,51 @@ class ArticlesController < ApplicationController
         stores << store[:id]
       end
     end
-    @stores = Store.find(stores.sort.uniq)
-    @near_stores = Store.near(search.input_address, search.distance, units: :km).where.not(latitude: nil, longitude: nil)
-    @stores.each do |store|
-      near_stores << store
-    end
-    stock_stores = @stores && near_stores
-    @markers = []
+    compute_stores_and_markers(@search, stores, nil)
+  end
 
+  def show
+    @article = Article.find(params[:id])
+    stores = []
+    @article.provider.stores.each do |store|
+      stores << store[:id]
+    end
+    compute_stores_and_markers(@search, stores, @article)
+    # fail
+  end
+
+
+  private
+
+  def set_search_and_near_stores
+    @search = Search.find(params[:search_id])
+    @near_stores = []
+    near_stores = Store.near(@search.input_address, @search.distance, units: :km)
+                       .where.not(latitude: nil, longitude: nil)
+    near_stores.each do |store|
+      @near_stores << store
+    end
+  end
+
+  def compute_stores_and_markers(search, stores, article)
+    stock_stores = []
+    @stores = Store.find(stores.sort.uniq)
+    stock_stores = @stores && @near_stores
+    unless article.nil?
+      stock_stores = stock_stores.select {|s| s.provider == article.provider}
+    end
+    # fail
+    @markers = []
+    @markers_stores = [] #les stores qui auront un marker sur la map
     # Fill Gmaps with stores having stock
     # @markers = @stores.map { |store| {lat: store.latitude, lng: store.longitude} }
     stock_stores.each do |store|
       next if store.provider == nil
       next if store.provider.articles == nil
+      @markers_stores << store
       @markers << {lat: store.latitude, lng: store.longitude}
     end
-
   end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-
-  private
 
   def params_article
     params.require(:article).permit(:title, :sku, :description, :price, :selected)
