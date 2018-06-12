67367cf3757bee83f7fa5ab7736a07c4b1648075
jeoffrey depinoy <jdepinoy@norauto.com>
2018-06-12 12:39:29 +0200

    itineraire

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,8 +3,9 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const showmap = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
+  window.map = showmap;
   map.addMarkers(markers);
   if (markers.length === 0) {
     map.setZoom(2);
@@ -22,3 +23,18 @@ autocomplete();
 // mapCard.addEventListener("mouseover", (event) => {
 //   console.log(event.srcElement);
 // });
+const showpage = document.getElementById('selsize');
+const store = JSON.parse(mapElement.dataset.store);
+const start = JSON.parse(mapElement.dataset.start);
+console.log(map)
+if (showpage != null) {
+  map.drawRoute({
+  origin: [start[0].lat, start[0].lng],
+  destination: [store[0].lat, store[0].lng],
+  travelMode: 'driving',
+  strokeColor: '#131540',
+  strokeOpacity: 0.6,
+  strokeWeight: 6
+  });
+}
+
