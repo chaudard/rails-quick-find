c86e51c475550e7506a714827d644f3b59010a0d
simon levy <simon94700@hotmail.fr>
2018-06-14 17:25:07 +0200

    last update for index

--- app/javascript/components/myBtn.js
+++ app/javascript/components/myBtn.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("Btnscroll").style.display = "block";
   } else {
-    document.getElementById("myBtn").style.display = "none";
+    document.getElementById("Btnscroll").style.display = "none";
   }
 }
 
