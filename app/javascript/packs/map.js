c7e3e9b371086377b8f11d84ede274501c09972b
jeoffrey depinoy <jdepinoy@norauto.com>
2018-06-14 10:15:19 +0200

    ajout tps et km sur show

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -156,7 +156,8 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
           storeSchedules.insertAdjacentHTML("afterbegin", schedule.name + ' : ' + schedule.open_hours + '<br>');
         }
       });
-      storeSchedules.insertAdjacentHTML("beforeend", 'Tel : ' + marker.phone + '<br>');
+      const storePhone = document.getElementById('store-phone');
+      storePhone.insertAdjacentHTML("beforeend", marker.phone + '<br>');
     }
   }
 
